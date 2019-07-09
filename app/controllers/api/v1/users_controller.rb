948bc29af2fffb4f98883ea84020813ff75470c0
Adam Sultanov <adam.sultanov@gmail.com>
2019-07-09 15:13:46 -0400

    t1513

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,31 +6,40 @@ class Api::V1::UsersController < ApplicationController
     render json: @users, status: 200
   end
 
-  def create
+  def login
 
-    @user = User.new
+    if User.find_by(username: params[:username])
 
-    @user.username = params[:username]
-    @user.password = params[:password]
-    @user.lat = params[:lat]
-    @user.lng = params[:lng]
+      @user = User.find_by(username: params[:username])
+      render json: @user, status: :accepted
 
-    ds_url = "https://api.darksky.net/forecast/"
-    secret_key = ENV['SECRET_KEY']
+    else
 
-    raw = RestClient.get("#{ds_url}#{secret_key}/#{@user.lat},#{@user.lng}?exclude=minutely,hourly,daily,alerts")
-    resp = JSON.parse(raw)
+      @user = User.new
 
-    if @user.save
-      if @user.create_weather({:temp => resp["currently"]["temperature"], :icon => resp["currently"]["icon"], :summary => resp["currently"]["summary"]})
-        puts "yay saved"
+      @user.username = params[:username]
+      @user.password = params[:password]
+      @user.lat = params[:lat]
+      @user.lng = params[:lng]
+
+      ds_url = "https://api.darksky.net/forecast/"
+      secret_key = ENV['SECRET_KEY']
+
+      raw = RestClient.get("#{ds_url}#{secret_key}/#{@user.lat},#{@user.lng}?exclude=minutely,hourly,daily,alerts")
+      resp = JSON.parse(raw)
+
+      if @user.save
+        if (@user.weather)
+          @user.weather.udpate({:temp => resp["currently"]["temperature"], :icon => resp["currently"]["icon"], :summary => resp["currently"]["summary"]})
+        else
+           @user.create_weather({:temp => resp["currently"]["temperature"], :icon => resp["currently"]["icon"], :summary => resp["currently"]["summary"]})
+        end
+
+        render json: @user, status: :accepted
       else
-        puts "oh no"
+        render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
       end
 
-      render json: @user, status: :accepted
-    else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
     end
   end
 
